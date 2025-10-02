39750d4d5eac30035b41ea67e718e5d9ca644d49
NikCodePro <vermanikhil7838@gmail.com>
2025-10-03 00:50:19 +0530

    host

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: '0.0.0.0',
     port: 8080,
+    allowedHosts: [
+      'artist.wedmacindia.com', // ✅ add your domain here
+      'www.artist.wedmacindia.com'
+    ]
   },
     build: {
     chunkSizeWarningLimit: 5000 // increase as needed
